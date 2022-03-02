fb79a0dd7d1e5caa11518880695d1e9afc748a8a
Charles-Nyabeze <charlesnnyabeze@gmail.com>
2022-03-01 23:15:54 -0500

    models

--- models/index.js
+++ models/index.js
@@ -1,3 +1,60 @@
 const User = require('./User');
+const Post = require("./Post");
+const Like = require('./Like');
+const Comment = require('./Comment');
 
-module.exports = { User };
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+  });
+
+User.belongsToMany(Post, {
+  through: Like,
+  as: 'liked_posts',
+  foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+  through: Like,
+  as: 'liked_posts',
+  foreignKey: 'post_id'
+});
+
+Like.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Like.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Like, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Like, {
+  foreignKey: 'post_id'
+});
+
+//ne
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Like, Comment };
