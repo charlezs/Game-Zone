9a5a0de795a99e826dd57236104f0b5512d8c30c
Habon Assoweh <habonassoweh@Habons-Air>
2022-03-06 00:01:50 -0500

    final updates to login/signup page

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const sign_in_btn = document.querySelector("#sign-in-button");
-const sign_up_btn = document.querySelector("sign-up-button");
+const sign_up_btn = document.querySelector("#sign-up-button");
 const container = document.querySelector(".container");
 
 sign_up_btn.addEventListener("click", () => {
