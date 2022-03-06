1d89c2f117bbd27e9b348f2318610cc525ab6270
jnnh <jenniferhorvath@cmail.carleton.ca>
2022-03-06 01:36:47 -0500

    dashboard fixed

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
-const dashboardRoutes = require('./dashboard-routes.js')
+const dashboardRoutes = require('./dashboard-routes.js');
 
-router.use('/dashboard', dashboardRoutes)
+router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 
