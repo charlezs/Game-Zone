a8dd7bbeb8316c4e54cdab360305f03a79e20519
jnnh <jenniferhorvath@cmail.carleton.ca>
2022-03-06 01:20:09 -0500

    initial functionality-dashboard errors

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -29,8 +29,6 @@ router.get('/', (req, res) => {
       ]
     })
       .then(dbPostData => {
-        // pass a single post object into the homepage template
-        console.log(dbPostData[0]);
         const posts = dbPostData.map(post => post.get({ plain: true }));
         res.render('homepage', {
           posts,
