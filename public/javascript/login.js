658d3dcd2c73d8cbb6117ab81c4a71fa1fbd3c77
jnnh <jenniferhorvath@cmail.carleton.ca>
2022-03-07 23:51:56 -0500

    Habon's login page combinded and working

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,3 +1,25 @@
+ //START OF HABON'S JS  
+const loginForm = document.querySelector("form.login");
+const signupForm = document.querySelector("form.signup");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector(".signup-link a");
+const loginText = document.querySelector(".title-text .login");
+const signupText = document.querySelector(".title-text .signup");
+signupBtn.onclick = () => {
+  loginForm.style.marginLeft = "-50%";
+  loginText.style.marginLeft = "-50%";
+};
+loginBtn.onclick = () => {
+  loginForm.style.marginLeft = "0%";
+  loginText.style.marginLeft = "0%";
+};
+signupLink.onclick = () => {
+  signupBtn.click();
+  return false;
+};
+ //END OF HABON'S JS  
+
 async function signupFormHandler(event) {
     event.preventDefault();
 
@@ -48,5 +70,5 @@ async function loginFormHandler(event) {
     }
   }
 
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+signupForm.addEventListener('submit', signupFormHandler);
+loginForm.addEventListener('submit', loginFormHandler);
