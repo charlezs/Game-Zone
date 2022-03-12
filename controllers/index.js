adf7ccd7ab9171043cff5d1c23908e6e0d60a3b5
jnnh <jenniferhorvath@cmail.carleton.ca>
2022-03-12 15:53:26 -0500

    final project

--- controllers/index.js
+++ controllers/index.js
@@ -7,8 +7,6 @@ router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 
-// router.use((req, res)=> {
-//     res.status(404).end();
-// });
+
 
 module.exports = router;
